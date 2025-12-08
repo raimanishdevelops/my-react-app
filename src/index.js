d30c34ea599091ee951d30e930d37cdec784224b
Manish <raiman9705@gmail.com>
2025-12-08 18:00:18 +0530

    Added comments to index.js file

--- src/index.js
+++ src/index.js
@@ -15,3 +15,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+  // Entry point of the project 
+  // there is this comment just o check show you the push and pull for any juniour developers that are watching
+  
